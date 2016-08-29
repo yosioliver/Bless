5db7eff4b648419646f51b8ff233bd42ab805342
bimbim <bhimbim_dahsyat@Yahoo.com>
2016-08-29 20:22:15 +0700

    SPAJ Beneficiaries List & SPAJ PDF.
    1. table setter getter.
    2. number for table.

--- Java Script/RAW/ibrahimaziz.js
+++ Java Script/RAW/ibrahimaziz.js
@@ -422,12 +422,23 @@ function popUpBeneficiariesListShow(stringKeyID)
         var stringInputJavaScriptID = $(this).attr("id");
 		var stringInputJQueryID = stringKres + stringInputJavaScriptID;
 		$(stringInputJQueryID).val("");
+		var stringInputIDWithoutPrefix;
+        var stringKey;
 		
-        var stringInputIDWithoutPrefix = stringInputJavaScriptID.substring(stringPrefixText.length, stringInputJavaScriptID.length);
-        var stringKey = stringPrefixText + stringBeneficiariesListInfix + stringInputIDWithoutPrefix + stringKeyID;
+		if (stringInputJavaScriptID.substring(0, stringPrefixDate.length) == stringPrefixDate)
+		{
+			stringInputIDWithoutPrefix = stringInputJavaScriptID.substring(stringPrefixDate.length, stringInputJavaScriptID.length);
+			stringKey = stringPrefixDate + stringBeneficiariesListInfix + stringInputIDWithoutPrefix + stringKeyID;
+		}
+		else
+		{
+			stringInputIDWithoutPrefix = stringInputJavaScriptID.substring(stringPrefixText.length, stringInputJavaScriptID.length);
+			stringKey = stringPrefixText + stringBeneficiariesListInfix + stringInputIDWithoutPrefix + stringKeyID;
+		}
+		
         var stringValue = arrayFind(arrayBeneficiariesList, stringKey);
         
-		alert("stringKey = " + stringKey + ", stringValue = " + stringValue);
+		// alert("stringKey = " + stringKey + ", stringValue = " + stringValue);
 		
         if (stringValue == null || stringValue == undefined)
         {
@@ -448,7 +459,7 @@ function popUpBeneficiariesListShow(stringKeyID)
         var stringKey = stringPrefixRadioButton + stringBeneficiariesListInfix + stringInputIDWithoutPrefix + stringKeyID;
         var stringValue = arrayFind(arrayBeneficiariesList, stringKey);
         
-		alert("stringKey = " + stringKey + ", stringValue = " + stringValue);
+		// alert("stringKey = " + stringKey + ", stringValue = " + stringValue);
 		
         if (stringValue == null || stringValue == undefined)
         {
@@ -469,7 +480,7 @@ function popUpBeneficiariesListShow(stringKeyID)
         var stringKey = stringPrefixSelect + stringBeneficiariesListInfix + stringInputIDWithoutPrefix + stringKeyID;
         var stringValue = arrayFind(arrayBeneficiariesList, stringKey);
         
-		alert("stringKey = " + stringKey + ", stringValue = " + stringValue);
+		// alert("stringKey = " + stringKey + ", stringValue = " + stringValue);
 		
         if (stringValue == null || stringValue == undefined)
         {
@@ -944,14 +955,15 @@ function buttonPopUpBeneficiariesListGenerator()
 					{
 						stringInputJavaScriptID = $(this).attr("id");
 						stringInputJQueryID = stringKres + stringInputJavaScriptID;
-						stringInputIDWithoutPrefix = stringInputJavaScriptID.substring(stringPrefixText.length, stringInputJavaScriptID.length);
 
-						if (stringInputJavaScriptID.substring(0, stringPrefixDate) == stringPrefixDate)
+						if (stringInputJavaScriptID.substring(0, stringPrefixDate.length) == stringPrefixDate)
 						{
+							stringInputIDWithoutPrefix = stringInputJavaScriptID.substring(stringPrefixDate.length, stringInputJavaScriptID.length);
 							stringKey = stringPrefixDate + stringBeneficiariesListInfix + stringInputIDWithoutPrefix + intBeneficiariesListID;
 						}
 						else
 						{
+							stringInputIDWithoutPrefix = stringInputJavaScriptID.substring(stringPrefixText.length, stringInputJavaScriptID.length);
 							stringKey = stringPrefixText + stringBeneficiariesListInfix + stringInputIDWithoutPrefix + intBeneficiariesListID;
 						}
 						
@@ -959,7 +971,6 @@ function buttonPopUpBeneficiariesListGenerator()
 
 						if (validateTextGeneral(stringInputJQueryID) == true)
 						{
-							alert("text = " + stringKey + " " + stringValue);
 							arrayAdd(arrayBeneficiariesList, stringKey, stringValue);
 						}
 						else
@@ -979,7 +990,6 @@ function buttonPopUpBeneficiariesListGenerator()
 
 						if (validateTextGeneral(stringInputJQueryID) == true)
 						{
-							alert("select = " + stringKey + " " + stringValue);
 							arrayAdd(arrayBeneficiariesList, stringKey, stringValue);
 						}
 						else
@@ -1000,7 +1010,6 @@ function buttonPopUpBeneficiariesListGenerator()
 
 						if (validateTextGeneral(stringInputJQueryID) == true)
 						{
-							alert("radiobutton = " + stringKey + " " + stringValue);
 							arrayAdd(arrayBeneficiariesList, stringKey, stringValue);
 						}
 						else
@@ -1462,6 +1471,22 @@ function getAreaPDF(stringID)
 }
 
 
+
+function numberGenerator(stringNameInfix, indexRow)
+{
+	var stringCellNumberJQueryID = stringKres + stringCellPrefix + stringNameInfix + stringNumberPrefix + indexRow;
+										
+	if ($(stringCellNumberJQueryID).text() == "" || $(stringCellNumberJQueryID).text() == undefined || $(stringCellNumberJQueryID).text() == null)
+	{
+		$(stringCellNumberJQueryID).append(indexRow + 1);
+	}
+	else
+	{
+
+	}
+}
+
+
 // GET FROM DATABASE
 
 function getFromDatabase(objectContent, stringPageType)
@@ -1527,6 +1552,11 @@ function getFromDatabase(objectContent, stringPageType)
 									if ($(stringCellJQueryID).text() == "" || $(stringCellJQueryID).text() == undefined || $(stringCellJQueryID).text() == null)
 									{
 										$(stringCellJQueryID).append(stringValue);
+										
+										// FOR NUMBER
+										
+										numberGenerator(stringProspectiveInsuredPrefix + "Illness", indexRow);
+										
 										return false;
 									}
 									else
@@ -1573,6 +1603,11 @@ function getFromDatabase(objectContent, stringPageType)
 								if ($(stringCellJQueryID).text() == "" || $(stringCellJQueryID).text() == undefined || $(stringCellJQueryID).text() == null)
 								{
 									$(stringCellJQueryID).append(stringValue);
+									
+									// FOR NUMBER
+										
+									numberGenerator(stringBeneficiariesListInfix, indexRow);
+									
 									return false;
 								}
 								else
